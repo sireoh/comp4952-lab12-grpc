79c16a2f99d3435e74ade45cc55753aeccabf0de
Vincent <eohndk@gmail.com>
2025-11-18 13:42:20 -0800

    initial commit

---
+++ GrpcStudentsClient/Program.cs
@@ -0,0 +1,61 @@
+﻿using Grpc.Net.Client;
+using GrpcStudentsClient;
+
+// The port number must match the port of the gRPC server.
+using var channel = GrpcChannel.ForAddress("http://localhost:5052");
+// var client = new Greeter.GreeterClient(channel);
+
+var sClient = new StudentRemote.StudentRemoteClient(channel);
+var sInput = new StudentLookupModel { StudentId = 3 };
+
+/*
+StudentModel sReply = await sClient.GetStudentInfoAsync(sInput);
+Console.WriteLine($"{sReply.StudentId}, {sReply.FirstName}  {sReply.LastName}, {sReply.School}");
+*/
+
+/*
+// ########################### Insert Student
+StudentModel newStudent = new StudentModel()
+{
+   FirstName = "Jim",
+   LastName = "Cox",
+   School = "Multimedia",
+};
+var replyNewStudent = sClient.InsertStudent(newStudent);
+Console.WriteLine($"Result={replyNewStudent.Result} & isOk={replyNewStudent.IsOk}");
+*/
+
+/*
+// ########################### Update Student
+StudentModel updateStudent = new StudentModel()
+{
+   StudentId = 5,
+   FirstName = "Jane",
+   LastName = "Jones",
+   School = "Pharmacy",
+};
+var replyUpdateStudent = sClient.UpdateStudent(updateStudent);
+Console.WriteLine($"Result={replyUpdateStudent.Result} & isOk={replyUpdateStudent.IsOk}");
+*/
+
+// ########################### Delete Student
+var deleteLookupModel = new StudentLookupModel();
+deleteLookupModel.StudentId = 5;
+var replyDeleteStudent = sClient.DeleteStudent(deleteLookupModel);
+
+
+// ########################### Display all students
+var replyAllStudents = sClient.RetrieveAllStudents(new Empty());
+foreach (var item in replyAllStudents.Items)
+{
+    Console.WriteLine($"{item.StudentId} {item.FirstName} {item.LastName} {item.School}");
+}
+
+
+
+// HelloReply reply = await client.SayHelloAsync(
+//                   new HelloRequest { Name = "Jane Bond" });
+// Console.WriteLine("Greeting: " + reply.Message);
+// Console.WriteLine("Press any key to exit...");
+
+Console.ReadLine();
